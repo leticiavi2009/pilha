04834001cfd98aef0dcf5c5cbdd8f926bdc56dfe
Leticia Vitoria Ribeiro Bastos <le01042009@gmail.com>
2025-12-09 16:14:30 -0300

    Add files via upload

--- countingDivisors.cpp
+++ countingDivisors.cpp
@@ -3,16 +3,26 @@
 using namespace std;
 
 int main(){
+    int contador = 0;
     int n;//variável inteiro
     cin >> n;//recebe informação do usuário
     vector <int> lista;
-    for(int i = 0; i < n; i++){ //quantidade de repetições
+    for(int i = 1; i < n+1; i++){ //quantidade de repetições  N 1e5
         int b;
         cin >> b;
         lista.push_back(b);
-
-
     }
-    cout << lista;
-    cout << a << ' ' << b << '\n';
+    
+   
+    for( int a = 0; a < n; a++){ // N 1e5
+        int contador = 0;
+        for(int i = 1; i <= lista[a];i++){ //Elemento 1e6
+            if (lista[a] % i == 0){
+                contador++;
+            }
+        }
+        cout << contador << "\n";
+    }
+    
+    
 }
