054c4241bc3e45a9b19216a6937c6d1c3b90e661
Leticia Vitoria Ribeiro Bastos <le01042009@gmail.com>
2025-12-09 11:39:11 -0300

    Add files via upload

---
+++ missing.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main{
+    int an;
+    int sumN = {(1+an)*an}/2;
+    int sumC = 0;
+    for {int i = 0; i < an-1 ; i++}
+    {
+        int curr;
+        
+    }
+
+}
